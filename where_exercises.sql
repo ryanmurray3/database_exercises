e61951d6732ed3244d240a529a76c95aa079bc30
RyanMurray <ryangmurray3@gmail.com>
2023-11-29 16:52:58 -0600

    part two of where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,8 +1,12 @@
 USE employees;
 
-SELECT emp_no, first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+SELECT * FROM employees WHERE first_name IN ('Vidya', 'Irena', 'Maya') AND gender = 'M' ORDER BY first_name;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e' ORDER BY last_name;
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
 SELECT * FROM employees WHERE LEFT(SUBSTRING_INDEX(last_name, ' ', -1), 1) = 'E';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
